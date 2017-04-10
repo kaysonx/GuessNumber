774d5176b8f472b12f7827cb63f5e7c87affb58a
utlight <joker_e@163.com>
2017-04-10 15:41:45 +0800

    add the printInfo

--- src/main/java/me/cyansong/guessnumber/GuessNumber.java
+++ src/main/java/me/cyansong/guessnumber/GuessNumber.java
@@ -4,20 +4,60 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GuessNumber {
+    public static void main(String[] args) {
+        int count = 0;
+        final int maxErrorCount = 5;
+        final int generatedNumber = getRandomNumber(1, 100);
+        //while (true){
+        //    if(maxErrorCount == count){
+        //        System.out.println("你已经猜错了"+maxErrorCount+"次，游戏结束！");
+        //        break;
+        //    }
+        //    System.out.println("请输入你猜的数字：");
+        //    int userInput = getInputFromConsole();
+        //    if(compareNumber(userInput, generatedNumber)){
+        //        System.out.println("恭喜你！猜中了，你一共猜了："+count+"次");
+        //    }else{
+        //        count++;
+        //        System.out.println("很遗憾，你猜错了！请重新输入猜的数字：");
+        //    }
+        //}
+    }
+
     public static int getRandomNumber(int min, int max){
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
     public static int getInputFromConsole(){
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        if (scanner.hasNextInt()){
+            return scanner.nextInt();
+        }else{
+            return -1;
+        }
     }
 
-    public static boolean compareNumber(int numberA,int numberB){
-        return numberA == numberB;
+    public static int compareNumber(int numberA,int numberB){
+        if(numberA < numberB){
+            return  -1;
+        }
+        if(numberA > numberB){
+            return 1;
+        }
+        return 0;
     }
 
     public static void print(String outputInfo){
         System.out.println(outputInfo);
+    }
+
+    public static String getPrintInfo(int compareResult){
+        if(compareResult < 0){
+            return "你猜的数太小了！";
+        }
+        if(compareResult > 0){
+            return  "你猜的数太大了！";
+        }
+        return "猜对了！";
     }
 }
--- src/test/java/GuessNumberTest.java
+++ src/test/java/GuessNumberTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static me.cyansong.guessnumber.GuessNumber.compareNumber;
+import static me.cyansong.guessnumber.GuessNumber.getPrintInfo;
 import static me.cyansong.guessnumber.GuessNumber.getRandomNumber;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -16,16 +17,43 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_compare_userInput_with_generated_number_when_not_equal(){
+    public void should_compare_userInput_with_generated_number_when_guessNumber_is_lessThan(){
         final int generatedNumber = 66;
         int userInput = 19;
-        assertEquals(compareNumber(generatedNumber,userInput),false);
+        assertEquals(compareNumber(userInput,generatedNumber),-1);
     }
 
     @Test
     public void should_compare_userInput_with_generated_number_when_is_equal(){
         final int generatedNumber = 66;
         int userInput = 66;
-        assertEquals(compareNumber(generatedNumber,userInput),true);
+        assertEquals(compareNumber(userInput,generatedNumber),0);
+    }
+
+    @Test
+    public void should_compare_userInput_with_generated_number_when_guessNumber_is_greaterThan(){
+        final int generatedNumber = 66;
+        int userInput = 77;
+        assertEquals(compareNumber(userInput,generatedNumber),1);
+    }
+
+    @Test
+    public void should_get_info_when_guessNumber_is_lessThan(){
+        String printInfo = getPrintInfo(-1);
+        String expectInfo = "你猜的数太小了！";
+        assertEquals(printInfo,printInfo);
+    }
+    @Test
+    public void should_get_info_when_guessNumber_is_greaterThan(){
+        String printInfo = getPrintInfo(1);
+        String expectInfo = "你猜的数太大了！";
+        assertEquals(printInfo,printInfo);
+    }
+
+    @Test
+    public void should_get_info_when_guessNumber_is_equal(){
+        String printInfo = getPrintInfo(0);
+        String expectInfo = "猜对了！";
+        assertEquals(printInfo,printInfo);
     }
 }
